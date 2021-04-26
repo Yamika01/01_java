083eb5c98db51f6ae8802308a941ff90ab8d3bd5
yamikaganji <83218365+yamikaganji@users.noreply.github.com>
2021-04-26 16:42:23 +0530

    first commit

---
+++ Sort.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+class Sort {
+	public static void main(String args[]) {
+		int numbers[] = { 43, 23, 54, 62, 10 };
+		System.out.print("the numbers before sorting are: " );
+		for(int index=0;index<numbers.length;index++) {
+			System.out.print(numbers[index]+" | ");
+		}
+		for (int rounds = 0; rounds < numbers.length; rounds++) {
+			for (int index = 0; index < numbers.length-1; index++) {
+				if (numbers[index] < numbers[index + 1]) {
+					int temp = numbers[index];
+					numbers[index] = numbers[index + 1];
+					numbers[index + 1] = temp;
+				}
+			}
+			
+		}
+		System.out.print("\nthe numbers after sorting  are: " );
+		for(int index=0;index<numbers.length;index++) {
+			System.out.print(numbers[index]+" | ");
+		}
+	
+	}
+}
